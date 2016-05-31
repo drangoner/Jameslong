dd386225fb9e7fb7d7f46d706b1d0bd59309d622
James long <1181281178@qq.com>
2016-05-31 14:24:26 +0800

    james

--- smtp/send_night.py
+++ smtp/send_night.py
@@ -21,16 +21,16 @@ to_addr =['1181281178@qq.com','2281582766@qq.com']
 smtp_server = 'smtp.163.com'
 
 msg = MIMEMultipart()
-msg['From'] = _format_addr('南山一颗树 <%s>' % from_addr)
-msg['To'] = _format_addr('Jameslong <%s>' % to_addr)
+msg['From'] = _format_addr('小园子 <%s>' % from_addr[0])
+msg['To'] = _format_addr('Jameslong <%s>' % to_addr[0])
 msg['Subject'] = Header('Good night …', 'utf-8').encode()
 
 msg.attach(MIMEText('<div style="background:url(http://d.hiphotos.baidu.com/zhidao/pic/item/c83d70cf3bc79f3d5b4ecce1bba1cd11738b29b7.jpg)"><h1 style="text-align:center;">Jameslong，Good night!</h1>' +'<p>因为有你，生活变得更加精彩！<br/>感谢你今天的存在！<br/>你的美梦即将到来！</p>'+ '<h1 style="text-align:center; font-size:32px;color:purple;padding-top:120px;">Good night！</h1></div>','html','utf-8'))
 
 
 msg_cq = MIMEMultipart()
-msg_cq['From'] = _format_addr('南山一颗树 <%s>' % from_addr)
-msg_cq['To'] = _format_addr('梦园 <%s>' % to_addr)
+msg_cq['From'] = _format_addr('南山一颗树 <%s>' % from_addr[0])
+msg_cq['To'] = _format_addr('梦园 <%s>' % to_addr[1])
 msg_cq['Subject'] = Header('Good night …', 'utf-8').encode()
 name = time.localtime()
 full_name =str(name[0]) + '-' + str(name[1]) + '-' + str(name[2]) + '.mp3'
@@ -42,6 +42,6 @@ server = smtplib.SMTP(smtp_server, 25)
 server.set_debuglevel(1)
 server.login(from_addr, password)
 server.sendmail(from_addr,['1181281178@qq.com'], msg.as_string())
-server.sendmail(from_addr,to_addr, msg_cq.as_string())
+server.sendmail(from_addr,['1181281178@qq.com'], msg_cq.as_string())
 server.quit()
 
