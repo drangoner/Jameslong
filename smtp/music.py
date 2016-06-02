947e9b1d702c6ce380316289504af4c40b63bdf6
James long <1181281178@qq.com>
2016-06-02 22:53:11 +0800

    james

--- smtp/music.py
+++ smtp/music.py
@@ -11,5 +11,5 @@ class music:
         name = time.localtime()
         full_name = '/var/www/friends/music/'+str(name[0])+'-'+str(name[1])+'-'+str(name[2])+'.mp3'
         urllib.request.urlretrieve(self.url,full_name)
-mu = music('218.197.116.217/m10.music.126.net/20160602231527/7e9d30ead0001c867d4ca6f350f91494/ymusic/7c61/821d/3107/62a76dbbfefaf0be8347bf24603c63df.mp3?wshc_tag=0&wsts_tag=575047b4&wsid_tag=decc0106&wsiphost=ipdbm')
+mu = music('http://218.197.116.217/m10.music.126.net/20160602231527/7e9d30ead0001c867d4ca6f350f91494/ymusic/7c61/821d/3107/62a76dbbfefaf0be8347bf24603c63df.mp3?wshc_tag=0&wsts_tag=575047b4&wsid_tag=decc0106&wsiphost=ipdbm')
 mu.download()
