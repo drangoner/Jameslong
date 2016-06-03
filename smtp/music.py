faee36aa07c2773c52c6f4801dad3eedbe0436db
James long <1181281178@qq.com>
2016-06-03 22:43:44 +0800

    james

--- smtp/music.py
+++ smtp/music.py
@@ -11,5 +11,5 @@ class music:
         name = time.localtime()
         full_name = '/var/www/friends/music/'+str(name[0])+'-'+str(name[1])+'-'+str(name[2])+'.mp3'
         urllib.request.urlretrieve(self.url,full_name)
-mu = music('http://218.197.116.217/m10.music.126.net/20160602231527/7e9d30ead0001c867d4ca6f350f91494/ymusic/7c61/821d/3107/62a76dbbfefaf0be8347bf24603c63df.mp3?wshc_tag=0&wsts_tag=575047b4&wsid_tag=decc0106&wsiphost=ipdbm')
+mu = music('http://222.204.1.115/20160603230726/0d96272d6c7669fd6de3c8dcbc82cabc/ymusic/bdb6/4eb6/aabd/26e833f3fe2e76237b2ef5f815077ffb.mp3?&ncs_hash_key_one=e6&ncs_hash_key_two=3b&ncs_hash_key=e63ba781365bde6ebd8a73c0d4560072&ncs_dir=0003/163cloudnmusic&')
 mu.download()
--- smtp/send_night.py
+++ smtp/send_night.py
@@ -27,8 +27,8 @@ msg['To'] = _format_addr('小鱼<%s>' % to_addr2)
 msg['Subject'] = Header('Good night …', 'utf-8').encode()
 name = time.localtime()
 full_name2 = str(name[0]) + '-' + str(name[1]) + '-' + str(name[2]) + '.mp3'
-lrc = "<div style='font-size:24px;color:#a0a0a0;height-light:32px;'><br>在我的怀里 在你的眼里<br>那里春风沉醉 那里绿草如茵<br>月光把爱恋 洒满了湖面<br>两个人的篝火 照亮整个夜晚<br>多少年以后 如云般游走<br>那变换的脚步 让我们难牵手<br>这一生一世 有多少你我<br>被吞没在月光如水的夜里<br><br>多想某一天 往日又重现<br>我们流连忘返 在贝加尔湖畔<br><br><br>多少年以后 往事随云走<br>那纷飞的冰雪容不下那温柔<br>这一生一世 这时间太少<br>不够证明融化冰雪的深情<br><br>就在某一天 你忽然出现<br>你清澈又神秘 在贝加尔湖畔<br>你清澈又神秘 像贝加尔湖畔<br><br></div>"
-msg.attach(MIMEText('<div style="background:url(http://img3.3lian.com/2014/c1/75/d/55.jpg );background-size:100% 100%; background-repeat: no-repeat;"><audio src="http://yukunlong.com:8080/music/' + full_name2 +'" controls="controls" autoplay="autoplay"><h1 style="text-align:center;color:#a0a0a0;">小鱼，Good night!</h1>' +lrc+ '<h1 style="text-align:center; font-size:32px;color:#f0f0f0;padding-top:320px;">Good night！</h1></div>','html','utf-8'))
+lrc = "<div style='font-size:24px;color:#a0a0a0;height-light:32px;'>你身上专属的陌生味道<br>是我确认你存在的目标<br>不用来回张望来知道<br>竟是我们相隔着一个街角<br><br>这么久了我还是可以看到<br>感觉得到你对我的重要<br>不会被天黑天亮打扰<br>你每一次的温柔我都想炫耀<br><br>我们绕了这么一圈才遇到<br>我比谁都更明白你的重要<br>这么久了我就决定了<br>决定了你的手我握了不会放掉<br><br>我们绕了这么一圈才遇到<br>我答应自己不再庸人自扰<br>因为我要的我自己知道<br>只要你的肩膀愿让我靠<br><br>你身上专属的陌生味道<br>是我确认你存在的目标<br>不用来回张望来知道<br>竟是我们相隔着一个街角<br><br>这么久了我还是可以看到<br>感觉得到你对我的重要<br>不会被天黑天亮打扰<br>你每一次的温柔我都想炫耀<br><br>我们绕了这么一圈才遇到<br>我比谁都更明白你的重要<br>这么久了我就决定了<br>决定了你的手我握了不会放掉<br><br>我们绕了这么一圈才遇到<br>我答应自己不再庸人自扰<br>因为我要的我自己知道<br>只要你的肩膀愿让我靠<br><br>这么久了我就决定了<br>决定了你的手我握了不会放掉<br>我们绕了这么一圈才遇到<br>我答应自己不再庸人自扰<br>因为我要的我自己知道<br>只要你的肩膀愿让我靠<br><br></div>"
+msg.attach(MIMEText('<div style="background:url(http://preview.quanjing.com/is026/is09a6j98.jpg );background-size:100% 100%; background-repeat: no-repeat;"><audio src="http://yukunlong.com:8080/music/' + full_name2 +'" controls="controls" autoplay="autoplay"><h1 style="text-align:center;color:#a0a0a0;">小鱼，Good night!</h1>' +lrc+ '<h1 style="text-align:center; font-size:32px;color:#f0f0f0;padding-top:320px;">Good night！</h1></div>','html','utf-8'))
 
 
 msg_cq = MIMEMultipart()
@@ -39,7 +39,7 @@ msg_cq['Subject'] = Header('Good night …', 'utf-8').encode()
 full_name =str(name[0]) + '-' + str(name[1]) + '-' + str(name[2]) + '.mp3'
 
 lrc_cq = "<div style='font-size:24px;color:#a0a0a0;height-light:32px;'><br>在我的怀里 在你的眼里<br>那里春风沉醉 那里绿草如茵<br>月光把爱恋 洒满了湖面<br>两个人的篝火 照亮整个夜晚<br>多少年以后 如云般游走<br>那变换的脚步 让我们难牵手<br>这一生一世 有多少你我<br>被吞没在月光如水的夜里<br><br>多想某一天 往日又重现<br>我们流连忘返 在贝加尔湖畔<br><br><br>多少年以后 往事随云走<br>那纷飞的冰雪容不下那温柔<br>这一生一世 这时间太少<br>不够证明融化冰雪的深情<br><br>就在某一天 你忽然出现<br>你清澈又神秘 在贝加尔湖畔<br>你清澈又神秘 像贝加尔湖畔<br><br></div>"
-msg_cq.attach(MIMEText('<div style="background:url(http://img3.3lian.com/2014/c1/75/d/55.jpg );background-size:100% auto; background-repeat: y-repeat;"><audio src="http://yukunlong.com:8080/music/' + full_name +'" controls="controls" autoplay="autoplay"><h1 style="text-align:center;color:#a0a0a0;">梦园，Good night!</h1>' +lrc_cq+ '<h1 style="text-align:center; font-size:32px;color:#f0f0f0;padding-top:320px;">Good night！</h1></div>','html','utf-8'))
+msg_cq.attach(MIMEText('<div style="background:url(http://preview.quanjing.com/is026/is09a6j98.jpg );background-size:100% 100%; background-repeat: no-repeat;"><audio src="http://yukunlong.com:8080/music/' + full_name +'" controls="controls" autoplay="autoplay"><h1 style="text-align:center;color:#a0a0a0;">梦园，Good night!</h1>' +lrc+ '<h1 style="text-align:center; font-size:32px;color:#f0f0f0;padding-top:320px;">Good night！</h1></div>','html','utf-8'))
 
 server = smtplib.SMTP(smtp_server, 25)
 server.set_debuglevel(1)
